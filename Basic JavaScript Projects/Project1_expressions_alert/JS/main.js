5d08d0d7c34d4d00fa3d58b031eb3cc320a9556f
Forto23 <fortfernando2328@gmail.com>
2025-08-14 11:01:56 -0700

    Add Project1_expressions_alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+// Show an alert
+window.alert("Hello! This is my first alert!");
+
+// Create two string variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Concatenate the strings and show them on the page
+document.write(Sent1 + Sent2);
+
+// Write a simple expression
+document.write(5 + 10);
+
+
+
+
+
+
+
+
