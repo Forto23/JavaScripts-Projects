f35680aa281529ad92c09c0bf05e9baca2d58aab
Forto23 <fortfernando2328@gmail.com>
2025-08-16 10:13:51 -0700

    Add Project5_comparisons_type_coercion

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,17 @@
+// variable
+let car = "Toyota 4Runner";
+
+// show type
+document.write(typeof car + "<br>");
+
+// string + number
+document.write("Year: " + 2018 + "<br>");
+
+// simple operators
+document.write(5=='5' + "<br>");
+document.write(5==='5' + "<br>");
+document.write(5>3 + "<br>");
+document.write(2<4 + "<br>");
+document.write(true&&false + "<br>");
+document.write(true||false + "<br>");
+document.write(!true + "<br>");
